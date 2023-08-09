a4f05d5f01511df2bd21bc92dd9adbc252c057a3
Muhammad Aadil <adil.isani1@gmail.com>
2023-08-09 22:19:22 +0500

    starting commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='nav'>
+                <div className='container-fluid'>
+                    <div className='left-nav'></div>
+                    <div className='center-nav'></div>
+                    <div className='right-nav'></div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
