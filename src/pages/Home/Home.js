e3865a6f7437ce885657691e5c86270d143e8619
Muhammad Adil <adil.isani1@gmail.com>
2023-08-10 10:37:35 +0500

    home-page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+import './Home.css';
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='container'>
+            <div className='shade-parent'>
+                <div className='shade'></div>
+            </div>
+            <div className='swap-card'>
+                <a className='card-anchor' href='#/swap'>
+                    <div className='card-section'>
+                        <main className='card-bg'>
+                            <div id="card-top" className='card-top-parent'>
+                                <div id="btns-top" className='top-btns'>
+                                    <span className='swap-btn'>Swap</span>
+                                    <div className='buy'>
+                                        <button id='btn-id' className='buy-btn'>Buy</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </main>
+                    </div>
+                </a>
+            </div>
+        </div>
     );
 }
 
